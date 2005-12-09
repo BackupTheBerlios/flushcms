1d48e7a683a912f32a0944b07b468dae8e5c52b9
arzen <arzen>
2005-12-09 03:40:32 +0000

    create config file

---
+++ flushcms_dev/Config/Config.php
@@ -0,0 +1,28 @@
+<?php
+/*
+   +----------------------------------------------------------------------+
+   | FlushPHP                                                             |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2005 The FlushPHP Group                                |
+   +----------------------------------------------------------------------+
+   | This library is free software; you can redistribute it and/or        |
+   | modify it under the terms of the GNU Lesser General Public           |
+   | License as published by the Free Software Foundation; either         |
+   | version 2.1 of the License, or (at your option) any later version.   |
+   +----------------------------------------------------------------------+
+   | Author: John.meng(ÃÏÔ¶òû)  Dec 9, 2005 9:07:01 AM                        
+   +----------------------------------------------------------------------+
+ */
+ 
+/* $Id: Config.php,v 1.1 2005/12/09 03:40:32 arzen Exp $ */
+
+// Below section is database setting
+$DB_Type = "mysql";
+$DB_Host = "localhost";
+$DB_UserName = "root";
+$DB_PassWord = "test1234";
+$DB_Name = "my_flushcms";
+
+$Themes = "default";// Set themes your can change it 
+
+?>
