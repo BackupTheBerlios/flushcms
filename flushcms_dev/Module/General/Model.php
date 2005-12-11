007ffde80fce23c52a87151bf6befec0a4895a13
arzen <arzen>
2005-12-11 15:06:01 +0000

    import quick form

--- flushcms_dev/Module/General/Model.php
+++ flushcms_dev/Module/General/Model.php
@@ -14,6 +14,25 @@
    +----------------------------------------------------------------------+
  */
  
-/* $Id: Model.php,v 1.1 2005/12/11 13:24:13 arzen Exp $ */
-$smarty->assign("Main",$MessageObj->displayMsg($__Lang__['langGeneralFileNotExist'],"MSG"));
+/* $Id: Model.php,v 1.2 2005/12/11 15:06:01 arzen Exp $ */
+include_once(PEAR_DIR."HTML/QuickForm.php");
+$form =& new HTML_QuickForm('frmTest', 'get');
+
+// Add some elements to the form
+$form->addElement('header', null, 'QuickForm tutorial example');
+$form->addElement('text', 'name', 'Enter your name:', array('size' => 50, 'maxlength' => 255));
+$form->addElement('submit', null, 'Send');
+$form->addElement('hidden', 'Model', 'General');
+$form->addElement('hidden', 'Page', 'Model');
+// Define filters and validation rules
+$form->applyFilter('name', 'trim');
+$form->addRule('name', 'Please enter your name', 'required');
+
+// Try to validate a form 
+if ($form->validate()) {
+    echo '<h1>Hello, ' . htmlspecialchars($form->exportValue('name')) . '!</h1>';
+
+}
+
+$smarty->assign("Main",$form->toHtml());
 ?>
