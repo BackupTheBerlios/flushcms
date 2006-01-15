d65c62cf82a441e056571a6bd8ba4b3cf45bb2bf
arzen <arzen>
2006-01-15 09:25:16 +0000

    remove system menu logic

--- flushcms_dev/dev-doc/database.sql
+++ flushcms_dev/dev-doc/database.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: flushcms
 ---------------------------------------------------------
--- Server version	4.0.5-beta-max-nt
+-- Server version	4.0.5-beta-max-nt-log
 
 --
 -- Current Database: flushcms
@@ -69,9 +69,9 @@ CREATE TABLE site_config (
 -- Dumping data for table 'site_config'
 --
 
-INSERT INTO site_config VALUES (1,2,'en-us','SITE_NAME','FlushPHP Office','2006-01-08 12:45:48','127.0.0.1');
-INSERT INTO site_config VALUES (2,2,'en-us','SITE_KEYWORD','shop;cms;php','2006-01-08 12:45:48','127.0.0.1');
-INSERT INTO site_config VALUES (3,2,'en-us','SITE_COPYRIGHT','CopyRight(c) 2005-2006 FlushPHP Group','2006-01-08 12:45:48','127.0.0.1');
+INSERT INTO site_config VALUES (1,2,'en-us','SITE_NAME','FlushPHP Office','2006-01-15 17:22:38','127.0.0.1');
+INSERT INTO site_config VALUES (2,2,'en-us','SITE_KEYWORD','shop;cms;php','2006-01-15 17:22:38','127.0.0.1');
+INSERT INTO site_config VALUES (3,2,'en-us','SITE_COPYRIGHT','CopyRight(c) 2005-2006 FlushPHP Group','2006-01-15 17:22:38','127.0.0.1');
 INSERT INTO site_config VALUES (4,2,'en-us','SITE_LOGO','Pic_43bf91eae27af.gif','2006-01-07 18:03:22','127.0.0.1');
 
 --
@@ -85,6 +85,8 @@ CREATE TABLE site_menu (
   VersionCode varchar(8) NOT NULL default '',
   Title varchar(200) NOT NULL default '',
   URL varchar(220) NOT NULL default '',
+  Module varchar(60) NOT NULL default '',
+  Template varchar(60) NOT NULL default '',
   CreateTime varchar(40) NOT NULL default '',
   AddIP varchar(24) NOT NULL default '',
   PRIMARY KEY  (SiteMenuID)
@@ -94,36 +96,11 @@ CREATE TABLE site_menu (
 -- Dumping data for table 'site_menu'
 --
 
-
---
--- Table structure for table 'sysmenu'
---
-
-CREATE TABLE sysmenu (
-  SysmenuID int(11) NOT NULL auto_increment,
-  PID int(6) NOT NULL default '0',
-  Title varchar(50) NOT NULL default '',
-  URL varchar(50) NOT NULL default '',
-  CreateTime datetime NOT NULL default '0000-00-00 00:00:00',
-  AddIP varchar(24) NOT NULL default '',
-  PRIMARY KEY  (SysmenuID)
-) TYPE=MyISAM COMMENT='System menu';
-
---
--- Dumping data for table 'sysmenu'
---
-
-INSERT INTO sysmenu VALUES (1,0,'test1','test1','2005-12-20 22:52:06','127.0.0.1');
-INSERT INTO sysmenu VALUES (2,0,'test2','test2','2005-12-20 22:52:19','127.0.0.1');
-INSERT INTO sysmenu VALUES (3,1,'test11','test11','2005-12-20 22:52:26','127.0.0.1');
-INSERT INTO sysmenu VALUES (4,3,'test111','test111','2005-12-20 22:52:36','127.0.0.1');
-INSERT INTO sysmenu VALUES (5,4,'test1111','test1111','2005-12-20 22:52:47','127.0.0.1');
-INSERT INTO sysmenu VALUES (6,2,'test21','test21','2005-12-20 22:52:59','127.0.0.1');
-INSERT INTO sysmenu VALUES (7,6,'test211','test211','2005-12-20 22:53:08','127.0.0.1');
-INSERT INTO sysmenu VALUES (8,7,'test2111','test2111','2005-12-20 22:53:18','127.0.0.1');
-INSERT INTO sysmenu VALUES (9,4,'test112','test112','2005-12-20 22:53:35','127.0.0.1');
-INSERT INTO sysmenu VALUES (10,3,'test122','test122','2005-12-20 22:53:55','127.0.0.1');
-INSERT INTO sysmenu VALUES (11,6,'test223','test223','2005-12-20 22:54:21','127.0.0.1');
+INSERT INTO site_menu VALUES (1,0,2,'en-us','Home','','ModuleHome','simple.info.index','1137311187','127.0.0.1');
+INSERT INTO site_menu VALUES (2,0,2,'en-us','DownLoad','','ModuleNews','simple.info.index','1137311574','127.0.0.1');
+INSERT INTO site_menu VALUES (3,2,2,'en-us','Document','','ModuleNews','simple.info.index','1137311580','127.0.0.1');
+INSERT INTO site_menu VALUES (4,0,2,'en-us','Forum','','ModuleNews','simple.info.index','1137311583','127.0.0.1');
+INSERT INTO site_menu VALUES (5,0,2,'en-us','FAQ','','ModuleNews','simple.info.index','1137311586','127.0.0.1');
 
 --
 -- Table structure for table 'ugroups'
