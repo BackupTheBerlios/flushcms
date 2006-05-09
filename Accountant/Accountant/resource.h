aa9fa590a96c1609fd7f72820517d42ec02402ab
arzen <arzen>
2006-05-09 14:20:48 +0000

    *** empty log message ***

--- Accountant/Accountant/resource.h
+++ Accountant/Accountant/resource.h
@@ -8,9 +8,9 @@
 #define ID_VIEW_ARRANGE                 127
 #define IDR_MAINFRAME                   128
 #define IDR_AccountantTYPE              129
-#define IDI_ICON1                       130
 #define IDI_ICON2                       131
 #define IDR_MENU1                       135
+#define IDI_ICON1                       139
 #define IDC_TYPE_NAME                   1000
 #define IDC_COMBO1                      1002
 #define IDC_P_TYPE                      1002
@@ -24,7 +24,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        137
+#define _APS_NEXT_RESOURCE_VALUE        140
 #define _APS_NEXT_COMMAND_VALUE         32781
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
