5617e7049cc067dbd5b3e5bc7ccd7e4685984577
arzen <arzen>
2006-08-04 23:25:37 +0000

    modi user module

--- solution/apps/backend/modules/users/actions/actions.class.php
+++ solution/apps/backend/modules/users/actions/actions.class.php
@@ -6,10 +6,34 @@
  * @package    solution
  * @subpackage users
  * @author     Your name here
- * @version    SVN: $Id: actions.class.php,v 1.1 2006/08/02 14:20:06 arzen Exp $
+ * @version    SVN: $Id: actions.class.php,v 1.2 2006/08/04 23:25:37 arzen Exp $
  */
 class usersActions extends autousersActions
 {
+
+  public function executeEdit()
+  {
+
+    // Finish off the edit screen in the usual way
+    parent::executeEdit();
+  }
+  
+  protected function updateUsersFromRequest()
+  {
+    $user_password = $this->getRequestParameter('password1');
+    $user_password2 = $this->getRequestParameter('password2');
+    $user_gender = $this->getRequestParameter('gender');
+    if ($user_password && ($user_password==$user_password2))
+    {
+      $this->users->setUserPwd($user_password);
+    }
+    if (isset($user_gender))
+    {
+      $this->users->setGender($user_gender);
+    }
+ 
+    parent::updateUsersFromRequest();
+  }
 
   public function handleError()
   {
---
+++ solution/apps/backend/modules/users/templates/_gender.php
@@ -0,0 +1,6 @@
+<?php 
+	echo radiobutton_tag('gender', 'M', false);
+?>
+<?=__("ÄÐ")?>
+<?php echo radiobutton_tag('gender', 'F', false) ?>
+<?=__("Å®")?>
