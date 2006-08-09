330ac42b25a57a7be2044c7192705be0723543b9
arzen <arzen>
2006-08-09 16:03:56 +0000

    modify question module etc.

--- solution/apps/backend/modules/question/actions/actions.class.php
+++ solution/apps/backend/modules/question/actions/actions.class.php
@@ -6,8 +6,26 @@
  * @package    solution
  * @subpackage question
  * @author     Your name here
- * @version    SVN: $Id: actions.class.php,v 1.1 2006/08/06 08:18:30 arzen Exp $
+ * @version    SVN: $Id: actions.class.php,v 1.2 2006/08/09 16:03:56 arzen Exp $
  */
 class questionActions extends autoquestionActions
 {
+  
+  protected function updateQuestionFromRequest()
+  {
+    $active = $this->getRequestParameter('active');
+    $category = $this->getRequestParameter('category');
+    if ($active) 
+    {
+		$this->question->setActive($active);
+	}
+    if ($category) 
+    {
+		$this->question->setCategory($category);
+	}
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$this->question->setAddIp($ip); 
+    parent::updateQuestionFromRequest();
+  }
+
 }
