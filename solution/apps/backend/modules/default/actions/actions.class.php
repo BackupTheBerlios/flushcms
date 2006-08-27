186a8543cdf39e5c4d651588eb94734660c1dc5a
arzen <arzen>
2006-08-27 23:23:30 +0000

    add defualt module

---
+++ solution/apps/backend/modules/default/actions/actions.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * default actions.
+ *
+ * @package    solution
+ * @subpackage default
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php,v 1.1 2006/08/27 23:23:30 arzen Exp $
+ */
+class defaultActions extends sfActions
+{
+  /**
+   * Executes index action
+   *
+   */
+  public function executeIndex()
+  {
+  }
+}
