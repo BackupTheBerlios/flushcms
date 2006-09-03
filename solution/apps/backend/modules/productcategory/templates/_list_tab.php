89cde2b9f006523003eb76c923ec5c4eb7471c83
arzen <arzen>
2006-09-03 14:54:36 +0000

    modify product category module

---
+++ solution/apps/backend/modules/productcategory/templates/_list_tab.php
@@ -0,0 +1,13 @@
+<?php
+
+	$top_tab = new sfTab();
+	$top_tab->setItem (__("企业名录列表"),'company');
+	$top_tab->setItem (__("添加企业"),'company/create');
+	$top_tab->setItem (__("产品列表"),'product');
+	$top_tab->setItem (__("添加产品"),'product/create');
+	$top_tab->setItem (__("产品分类列表"),'productcategory');
+	$top_tab->setItem (__("添加产品分类"),'productcategory/create');
+	$top_tab->setDefaultItem (__("产品分类列表"));
+	$top_tab->renderHtml();
+
+?>
