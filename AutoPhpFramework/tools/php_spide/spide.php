545819f4c1aff4cefd85f687e84cafddfa00cac7
arzen <arzen>
2006-10-18 06:08:26 +0000

    add log and mail library

---
+++ AutoPhpFramework/tools/php_spide/spide.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * spide.php
+ *
+ * @package    core
+ * @author     John.meng <arzen1013@gmail.com>
+ * @author     ÃÏÔ¶òû
+ * @author     QQ:3440895
+ * @version    CVS: $Id: spide.php,v 1.1 2006/10/18 06:08:26 arzen Exp $
+ */
+define('APF_ROOT_DIR',    realpath(dirname(__FILE__).'/../..'));
+require_once(APF_ROOT_DIR.DIRECTORY_SEPARATOR.'init.php');
+
+?>
