5e252e7188a558c0f7dfcb020231fdaeb799ca45
arzen <arzen>
2006-10-18 11:06:26 +0000

    modify php spide tool

--- AutoPhpFramework/tools/php_spide/spide.php
+++ AutoPhpFramework/tools/php_spide/spide.php
@@ -7,7 +7,7 @@
  * @author     John.meng <arzen1013@gmail.com>
  * @author     ÃÏÔ¶òû
  * @author     QQ:3440895
- * @version    CVS: $Id: spide.php,v 1.3 2006/10/18 10:41:50 arzen Exp $
+ * @version    CVS: $Id: spide.php,v 1.4 2006/10/18 11:06:26 arzen Exp $
  */
 define('APF_ROOT_DIR',    realpath(dirname(__FILE__).'/../..'));
 require_once(APF_ROOT_DIR.DIRECTORY_SEPARATOR.'init.php');
@@ -17,6 +17,7 @@ require_once 'XML/Util.php';
 require_once 'ConfigFile.php';
 require_once 'ContentParse.php';
 require_once 'Log.php';
+require_once 'Spreadsheet/Excel/Writer.php';
 
 $PattenConfigDir = "config/";
 if (!file_exists($PattenConfigDir)) 
@@ -32,6 +33,13 @@ if (!file_exists($PattenLogDir))
 	
 }
 
+$PattenDataDir = "data/";
+if (!file_exists($PattenDataDir)) 
+{
+	mkdir($PattenDataDir,0777);
+	
+}
+
 //http://www.sznet.com.cn/company_contact.php?userid=683
 $site="www.sznet.com.cn";
 $url="http://www.sznet.com.cn/company_contact.php?userid=683";
@@ -40,18 +48,66 @@ $file_type = "GenericConf";
 $conf = array('mode' => 0777, 'timeFormat' => '%X %x');
 $logger = &Log::singleton('file', $PattenLogDir.$site.'.log', 'ident', $conf);
 
+// Creating a workbook
+$workbook = new Spreadsheet_Excel_Writer();
+// sending HTTP headers
+$filename = $PattenDataDir.$site.date("Y_m_d").".xls";
+$workbook->send($filename);
+// Creating a worksheet
+$worksheet =& $workbook->addWorksheet(date("Y_m_d"));
+
+
 //createConfigFile($site,$file_type);
  
 $data = readConfigFile($site,$file_type); 
-$content = getContent($url);
 
-//var_dump($data);
-foreach($data["root"] as $key=>$value)
+// Write header
+$i=0;
+foreach($data["root"] as $title=>$lenght)
 {
-	if ( $value != "" ) 
+	$format_title =& $workbook->addFormat(array('right' => 5, 'top' => 20, 'size' => 14,
+                                              'pattern' => 1, 'bordercolor' => 'blue',
+                                              'Bold '=>1,'Color'=>'yellow','Align'=>'center',
+                                              'fgcolor' => 'blue'));
+	$worksheet->setInputEncoding('gb2312');
+	$worksheet->write(0, $i, $title,$format_title);
+	$worksheet->setColumn($i,$i,30);
+	$i++;
+}
+
+// write data
+$x=1;
+for ($m=681;$m<685;$m++)
+{
+	$url="http://www.sznet.com.cn/company_contact.php?userid=".$m;
+	$content = getContent($url);
+	if ( $content != "" ) 
 	{
-		var_dump(parseTag ($value,$content));
+		
+		$y=0;
+		foreach($data["root"] as $key=>$value)
+		{
+			$coloum_text = parseTag ($value,$content);
+			$worksheet->setInputEncoding('gb2312');
+			$worksheet->write($x, $y, $coloum_text);
+			$y++;
+		}
+		$x++;
+		echo "{$x} ok.\n";
 	}
 }
+		
+$worksheet->freezePanes(array(1, 1));
+		
+$workbook->close();
+
+//var_dump($data);
+//foreach($data["root"] as $key=>$value)
+//{
+//	if ( $value != "" ) 
+//	{
+//		var_dump(parseTag ($value,$content));
+//	}
+//}
 
 ?>
