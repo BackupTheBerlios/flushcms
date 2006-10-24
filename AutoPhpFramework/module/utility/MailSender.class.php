300124996ddc89435cb75e77817892976cee2dcf
arzen <arzen>
2006-10-24 02:06:32 +0000

    add mail sender utility

---
+++ AutoPhpFramework/module/utility/MailSender.class.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ *
+ * MailSender.class.php
+ *
+ * @package    core
+ * @author     John.meng <arzen1013@gmail.com>
+ * @author     ÃÏÔ¶òû
+ * @author     QQ:3440895
+ * @version    CVS: $Id: MailSender.class.php,v 1.1 2006/10/24 02:06:32 arzen Exp $
+ */
+
+class MailSender
+{
+
+	function executeWriteMail()
+	{
+		global $template,$WebBaseDir,$controller,$i18n,$ActiveOption;
+		$template->setFile(array (
+			"MAIN" => "apf_mail_write.html"
+		));
+		$template->setBlock("MAIN", "edit_block");
+		
+		$template->setVar(array (
+			"WEBDIR" => $WebBaseDir,
+			"TEXTAREACONTENT" => textareaTag ("content","",true),
+			"DOACTION" => "addsubmit"
+		));
+	
+	}
+	
+}
+?>
