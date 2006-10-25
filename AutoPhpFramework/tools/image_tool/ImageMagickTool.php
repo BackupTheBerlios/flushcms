0daa029295e9469044c013570cb0057cd4b8db20
arzen <arzen>
2006-10-25 10:50:47 +0000

    add imagemagick utility module

--- AutoPhpFramework/tools/image_tool/ImageMagickTool.php
+++ AutoPhpFramework/tools/image_tool/ImageMagickTool.php
@@ -7,7 +7,7 @@
  * @author     John.meng <arzen1013@gmail.com>
  * @author     ÃÏÔ¶òû
  * @author     QQ:3440895
- * @version    CVS: $Id: ImageMagickTool.php,v 1.1 2006/10/25 10:33:23 arzen Exp $
+ * @version    CVS: $Id: ImageMagickTool.php,v 1.2 2006/10/25 10:50:47 arzen Exp $
  */
 set_time_limit(0);
 define('APF_ROOT_DIR', realpath(dirname(__FILE__) . '/../..'));
@@ -18,6 +18,7 @@ require_once $ClassDir.'ImageMagickUtility.class.php';
 $source_name = 'C:/Documents and Settings/All Users/Documents/My Pictures/Sample Pictures/Water lilies.jpg';
 $string = "testing";
 $save_name = "D:/www/tools/Water lilies.jpg";
-ImageMagickUtility::addText($source_name,$string,$save_name);
+//ImageMagickUtility::addText($source_name,$string,$save_name);
+ImageMagickUtility::resize($source_name,60);
 
 ?>
