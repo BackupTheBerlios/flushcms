7f261795bfab72cf6aef6ec540f2117d65f9ed4f
arzen <arzen>
2006-11-03 04:50:38 +0000

    modify UI

--- YWeb/web/front.php
+++ YWeb/web/front.php
@@ -7,10 +7,10 @@
  * @author     John.meng <arzen1013@gmail.com>
  * @author     √œ‘∂Ú˚
  * @author     QQ:3440895
- * @version    CVS: $Id: front.php,v 1.2 2006/11/03 00:01:58 arzen Exp $
+ * @version    CVS: $Id: front.php,v 1.3 2006/11/03 04:50:38 arzen Exp $
  */
 define('APF_ROOT_DIR',    realpath(dirname(__FILE__).'/..'));
-define('APF_DEBUG',       true);
+define('APF_DEBUG',       false);
 require_once(APF_ROOT_DIR.DIRECTORY_SEPARATOR.'front_init.php');
 $DefaultModule = "default";
 $DefaultPage = "index";
