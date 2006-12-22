7874ea95c9320e2674ee624a61615ae8f2862295
arzen <arzen>
2006-12-22 10:25:58 +0000

    add company tab

--- phpGUI/ycrmgui/form/yc_company.form.inc.php
+++ phpGUI/ycrmgui/form/yc_company.form.inc.php
@@ -7,15 +7,16 @@
  * @author     John.meng <arzen1013@gmail.com>
  * @author     ÃÏÔ¶òû
  * @author     QQ:3440895
- * @version    CVS: $Id: yc_company.form.inc.php,v 1.1 2006/12/22 10:09:43 arzen Exp $
+ * @version    CVS: $Id: yc_company.form.inc.php,v 1.2 2006/12/22 10:25:58 arzen Exp $
  */
 function displayCompanyMainTabForm () 
 {
 	global $wb;
 	
 	include(PATH_FORM."yc_company.form.php");
+	$wb->right_control = $maintab = $tab;
+	include(PATH_FORM."yc_company_tab.form.php");
 	
-	$wb->right_control = $tab;
 	
 }
 ?>
