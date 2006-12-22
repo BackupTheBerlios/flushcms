29f96660191b2331bfb4539be83cb86f75f77131
arzen <arzen>
2006-12-22 10:39:40 +0000

    modify company tab

--- phpGUI/ycrmgui/form/yc_company.form.inc.php
+++ phpGUI/ycrmgui/form/yc_company.form.inc.php
@@ -7,7 +7,7 @@
  * @author     John.meng <arzen1013@gmail.com>
  * @author     ÃÏÔ¶òû
  * @author     QQ:3440895
- * @version    CVS: $Id: yc_company.form.inc.php,v 1.2 2006/12/22 10:25:58 arzen Exp $
+ * @version    CVS: $Id: yc_company.form.inc.php,v 1.3 2006/12/22 10:39:40 arzen Exp $
  */
 function displayCompanyMainTabForm () 
 {
@@ -17,6 +17,15 @@ function displayCompanyMainTabForm ()
 	$wb->right_control = $maintab = $tab;
 	include(PATH_FORM."yc_company_tab.form.php");
 	
+	wb_set_text(wb_get_control($maintab,IDC_COMPANY_LIST), array(
+	   array($wb->vars["Lang"]["lang_id"], 	60),
+	   array($wb->vars["Lang"]["lang_name"],150),
+	   array($wb->vars["Lang"]["lang_gender"],	40),
+	   array($wb->vars["Lang"]["lang_phone"],	100),
+	   array($wb->vars["Lang"]["lang_mobile"],	100),
+	   array($wb->vars["Lang"]["lang_email"],	180),
+	   array($wb->vars["Lang"]["lang_addrees"],	200),
+	));
 	
 }
 ?>
